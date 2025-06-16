3dfada99d021077d1c93a349ed7d8782c7397498
Fproano <flo.proano@gmail.com>
2025-06-16 10:16:18 +0200

    add react

--- resources/views/account/dashboard.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h2 class="text-2xl font-bold mb-6">Mon compte</h2>
-    <p>Bienvenue dans votre espace personnel.</p>
-    {{-- Ici tu peux afficher les commandes, infos utilisateur, etc. --}}
-@endsection
